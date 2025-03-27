b2312c943672c0534478ddcab12cbf426d832e60
Miguel Ángel <ma.rodgar1999@gmail.com>
2025-03-27 19:04:27 +0100

    Update

--- src/A-star C/AStar.c
+++ src/A-star C/AStar.c
@@ -8,8 +8,9 @@ typedef struct __NeighborRecord *NeighborRecord;
 typedef struct __OpenSet *OpenSet;
 typedef struct __ClosedSet *ClosedSet;
 
+static const NeighborRecord NullNeighbor = {NULL, -1};
+
 struct __Node {
-    size_t nodeSize;
     float h_value;
     float g_value;
     unsigned isOpen:1;
@@ -18,20 +19,20 @@ struct __Node {
 };
 
 struct __NeighborsList {
-    size_t capacity;
-    size_t count;
-    NeighborRecord* list;
+    int capacity;
+    int count;
+    NeighborRecord *list;
 };
 
 struct __NeighborRecord {
     float cost;
     Node node;
+    NeighborRecord next;
 };
 
 Node CreateNode(float hValue, float gValue)
 {
     Node n = malloc(sizeof(Node));
-    n->nodeSize = sizeof(Node);
     n->h_value = hValue;
     n->g_value = gValue;
     n->isOpen = 0;
@@ -39,51 +40,52 @@ Node CreateNode(float hValue, float gValue)
     n->neighbors = malloc(sizeof(NeighborsList));
     n->neighbors->capacity = 1;
     n->neighbors->count = 0;
-    n->neighbors->list = malloc(sizeof(NeighborRecord));
     return n;
 }
 
 void DestroyNode(Node n) {
-    free(n->neighbors->list);
-    free(n->neighbors);
     free(n);
 }
 
 void AddNeighbor(Node n, Node neighbor, float cost)
-{
-    NeighborRecord nr;
+{   
+    NeighborRecord nr = malloc(sizeof(NeighborRecord));
     nr->cost = cost;
     nr->node = neighbor;
-    
+
     if (n->neighbors->count == n->neighbors->capacity) {
         n->neighbors->capacity = 1 + (n->neighbors->capacity * 2);
         n->neighbors->list = realloc(n->neighbors->list, n->neighbors->capacity * sizeof(NeighborRecord));
     }
-    n->neighbors->list[n->neighbors->count++] = nr;
+
+    int pos = 0;
+    for (int i = 0; i < n->neighbors->count; i++) {
+        if (n->neighbors->list[i]->cost <= nr->cost) {
+            pos += 1;
+        }
+    }
+    
+    if (n->neighbors->list[pos] != NullNeighbor) {
+        n->neighbors->list[pos]->next = nr;
+    }
+    nr->next = n->neighbors->list[pos+1];
+    
 }
 
 int main(int argc, char const *argv[])
 {
-    Node n1 = CreateNode(0, 2);
+    Node n1 = CreateNode(1, 1);
     Node n2 = CreateNode(1, 1);
-    Node n3 = CreateNode(2, 0);
-    printf("Node 1:\n");
-    printf("  hValue = %f\n", n1->h_value);
-    printf("  gValue = %f\n", n1->g_value);
-    printf("  Node size = %zu\n", n1->nodeSize);
-    printf("\nNode 2:\n");
-    printf("  hValue = %f\n", n2->h_value);
-    printf("  gValue = %f\n", n2->g_value);
-    printf("  Node size = %zu\n", n2->nodeSize);
-    printf("\nNode 3:\n");
-    printf("  hValue = %f\n", n3->h_value);
-    printf("  gValue = %f\n", n3->g_value);
-    printf("  Node size = %zu\n", n3->nodeSize);
+    Node n3 = CreateNode(1, 1);
+    Node n4 = CreateNode(1, 1);
+    Node n5 = CreateNode(1, 1);
+    Node n6 = CreateNode(1, 1);
     
-    AddNeighbor(n1, n2, 2);
-    printf("\nAdded n2 as neighbor to n1 with cost %f\n", n1->neighbors->list[0]->cost);
-    AddNeighbor(n2, n3, 3);
-    printf("\nAdded n3 as neighbor to n2 with cost %f\n", n2->neighbors->list[0]->cost);
+    AddNeighbor(n1, n2, 1);
+    AddNeighbor(n1, n3, 2);
+    AddNeighbor(n1, n4, 2);
+    AddNeighbor(n1, n5, 3);
+    AddNeighbor(n1, n6, 1);
     
     
     DestroyNode(n1);
