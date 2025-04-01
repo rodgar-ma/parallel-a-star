5f6e5ebeada6040972f9e09d9461b84cb4fd1224
Miguel Ángel Rodríguez García <ma.rodgar1999@gmail.com>
2025-04-01 18:37:37 +0200

    Update AStar.c

--- src/AStar test/AStar.c
+++ src/AStar test/AStar.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "AStar.h"
 
+typedef struct __Neighbor *Neighbor;
 
 struct __NeighborsList {
     int count;
@@ -10,18 +11,18 @@ struct __NeighborsList {
 struct __Neighbor {
     float cost;
     void *node;
+    Neighbor next;
 };
 
-NeighborsList CreateNeighborsList(const AStarSource * source) {
-    NeighborsList nl = malloc(sizeof(struct __NeighborsList));
-    nl->capacity = 0;
-
+Neighbor CreateNeighbor(void *node, float cost) {
+    Neighbor nb = malloc(sizeof(struct __Neighbor));
+    nb->cost = cost;
+    nb->node = node;
 }
 
-void AddNeighbor(NeighborsList neighbors, void *node) {
-    if (!neighbors) {
+void AddNeighbor(NeighborsList neighbors, void *node, float cost) {
+    Neighbor nb = CreateNeighbor(node, cost);
 
-    }
 }
 
 
